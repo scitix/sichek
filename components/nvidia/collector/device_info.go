59f4e2933ab113f3849ed07e61fe2a69e4c1f8d7
xlliu-scitix <xlliu@scitix.ai>
2025-11-14 06:46:21 +0000

    fix(gpuevents, nvidia): fix index out of range panic when GPU card fails fix(nvidia): continue collecting metrics even when some metrics Get() fails

--- components/nvidia/collector/device_info.go
+++ components/nvidia/collector/device_info.go
@@ -28,21 +28,22 @@ import (
 )
 
 type DeviceInfo struct {
-	Name         string          `json:"name" yaml:"name"`
-	Index        int             `json:"Index" yaml:"Index"`
-	UUID         string          `json:"uuid" yaml:"uuid"`
-	Serial       string          `json:"serial" yaml:"serial"`
-	VBIOSVersion string          `json:"vbios_version" yaml:"vbios_version"`
-	PCIeInfo     PCIeInfo        `json:"pcie_info" yaml:"pcie_info"`
-	States       StatesInfo      `json:"states_info" yaml:"states_info"`
-	Clock        ClockInfo       `json:"clock_info" yaml:"clock_info"`
-	ClockEvents  ClockEvents     `json:"clock_events" yaml:"clock_events"`
-	Power        PowerInfo       `json:"power_info" yaml:"power_info"`
-	Temperature  TemperatureInfo `json:"temperature_info" yaml:"temperature_info"`
-	Utilization  UtilizationInfo `json:"utilization_info" yaml:"utilization_info"`
-	NVLinkStates NVLinkStates    `json:"nvlink_state" yaml:"nvlink_state"`
-	MemoryErrors MemoryErrors    `json:"ecc_event" yaml:"ecc_event"`
-	NProcess     int             `json:"nprocess" yaml:"nprocess"`
+	Name          string          `json:"name" yaml:"name"`
+	Index         int             `json:"Index" yaml:"Index"`
+	UUID          string          `json:"uuid" yaml:"uuid"`
+	Serial        string          `json:"serial" yaml:"serial"`
+	VBIOSVersion  string          `json:"vbios_version" yaml:"vbios_version"`
+	PCIeInfo      PCIeInfo        `json:"pcie_info" yaml:"pcie_info"`
+	States        StatesInfo      `json:"states_info" yaml:"states_info"`
+	Clock         ClockInfo       `json:"clock_info" yaml:"clock_info"`
+	ClockEvents   ClockEvents     `json:"clock_events" yaml:"clock_events"`
+	Power         PowerInfo       `json:"power_info" yaml:"power_info"`
+	Temperature   TemperatureInfo `json:"temperature_info" yaml:"temperature_info"`
+	Utilization   UtilizationInfo `json:"utilization_info" yaml:"utilization_info"`
+	NVLinkStates  NVLinkStates    `json:"nvlink_state" yaml:"nvlink_state"`
+	MemoryErrors  MemoryErrors    `json:"ecc_event" yaml:"ecc_event"`
+	NProcess      int             `json:"nprocess" yaml:"nprocess"`
+	PartialErrors []string        `json:"partial_errors,omitempty" yaml:"partial_errors,omitempty"`
 }
 
 func (deviceInfo *DeviceInfo) JSON() ([]byte, error) {
@@ -55,10 +56,12 @@ func (deviceInfo *DeviceInfo) ToString() string {
 }
 
 func (deviceInfo *DeviceInfo) Get(device nvml.Device, index int, driverVersion string) error {
+	deviceInfo.PartialErrors = make([]string, 0)
+
 	// Get GPU Name
 	name, err := device.GetName()
 	if !errors.Is(err, nvml.SUCCESS) {
-		return fmt.Errorf("failed to get name for GPU %d: %v", index, nvml.ErrorString(err))
+		deviceInfo.PartialErrors = append(deviceInfo.PartialErrors, fmt.Sprintf("failed to get gpu name: %v", nvml.ErrorString(err)))
 	}
 	deviceInfo.Name = name
 
@@ -66,94 +69,110 @@ func (deviceInfo *DeviceInfo) Get(device nvml.Device, index int, driverVersion s
 	// moduleId, err := device.GetModuleId()
 	minorNumber, err := device.GetMinorNumber()
 	if !errors.Is(err, nvml.SUCCESS) {
-		return fmt.Errorf("failed to get index for GPU %d: %v", index, nvml.ErrorString(err))
+		deviceInfo.PartialErrors = append(deviceInfo.PartialErrors, fmt.Sprintf("failed to get gpu index: %v", nvml.ErrorString(err)))
 	}
 	deviceInfo.Index = minorNumber
 
 	// Get GPU UUID
 	uuid, err := device.GetUUID()
 	if !errors.Is(err, nvml.SUCCESS) {
-		return fmt.Errorf("failed to get UUID for GPU %d: %v", index, nvml.ErrorString(err))
+		deviceInfo.PartialErrors = append(deviceInfo.PartialErrors, fmt.Sprintf("failed to get gpu uuid: %v", nvml.ErrorString(err)))
 	}
 	deviceInfo.UUID = uuid
 
 	// Get GPU Serial
 	serial, err := device.GetSerial()
 	if !errors.Is(err, nvml.SUCCESS) {
-		return fmt.Errorf("failed to get serial for GPU %v: %v", uuid, nvml.ErrorString(err))
+		deviceInfo.PartialErrors = append(deviceInfo.PartialErrors, fmt.Sprintf("failed to get serial: %v", nvml.ErrorString(err)))
+	} else {
+		deviceInfo.Serial = serial
 	}
-	deviceInfo.Serial = serial
 
 	// Get the VBIOS version, may differ between GPUs
 	vbiosVersion, err := device.GetVbiosVersion()
 	if !errors.Is(err, nvml.SUCCESS) {
-		return fmt.Errorf("failed to get VBIOS version for device 0: %v", nvml.ErrorString(err))
+		deviceInfo.PartialErrors = append(deviceInfo.PartialErrors, fmt.Sprintf("failed to get VBIOS version: %v", nvml.ErrorString(err)))
+	} else {
+		deviceInfo.VBIOSVersion = vbiosVersion
 	}
-	deviceInfo.VBIOSVersion = vbiosVersion
 
+	// Get States info
 	err2 := deviceInfo.States.Get(device, uuid)
 	if err2 != nil {
-		return fmt.Errorf("failed to get states info for device %v: %v", uuid, err2)
+		deviceInfo.PartialErrors = append(deviceInfo.PartialErrors, fmt.Sprintf("failed to get states info: %v", err2))
 	}
 
+	// Get PCIe info
 	err2 = deviceInfo.PCIeInfo.Get(device, uuid)
 	if err2 != nil {
-		return fmt.Errorf("failed to get pcie info for device %v: %v", uuid, err2)
+		deviceInfo.PartialErrors = append(deviceInfo.PartialErrors, fmt.Sprintf("failed to get pcie info: %v", err2))
 	}
 
+	// Get Clock info
 	err2 = deviceInfo.Clock.Get(device, uuid)
 	if err2 != nil {
-		return fmt.Errorf("failed to get clock %v: %v", uuid, err2)
+		deviceInfo.PartialErrors = append(deviceInfo.PartialErrors, fmt.Sprintf("failed to get clock info: %v", err2))
 	}
 
 	// clock events are supported in version 535 and above
 	// otherwise, the function GetCurrentClocksEventReasons() will exits with undefined symbol: nvmlGetCurrentClocksEventReasons
 	isSupported, err3 := isDriverVersionSupportedClkEvents(driverVersion, 535)
 	if err3 != nil {
-		logrus.WithField("component", "nvidia-collector-device-info").Warnf("failed to check if driver version %v is supported for clock events: %v", driverVersion, err2)
+		logrus.WithField("component", "nvidia-collector-device-info").Warnf("failed to check if driver version %v is supported for clock events: %v", driverVersion, err3)
 	}
 	deviceInfo.ClockEvents.IsSupported = isSupported
 	if isSupported {
 		err2 = deviceInfo.ClockEvents.Get(device, uuid)
 		if err2 != nil {
-			return fmt.Errorf("failed to get clock events for device %v: %v", uuid, err2)
+			deviceInfo.PartialErrors = append(deviceInfo.PartialErrors, fmt.Sprintf("failed to get clock events: %v", err2))
 		}
 	}
 
+	// Get Power info
 	err2 = deviceInfo.Power.Get(device, uuid)
 	if err2 != nil {
-		return fmt.Errorf("failed to get Power for device %v: %v", uuid, err2)
+		deviceInfo.PartialErrors = append(deviceInfo.PartialErrors, fmt.Sprintf("failed to get power info: %v", err2))
 	}
 
+	// Get Temperature info (skip for L40)
 	deviceID := fmt.Sprintf("0x%x", deviceInfo.PCIeInfo.DEVID)
 	if deviceID != "0x26b510de" { // skip temperature events for L40
 		err2 = deviceInfo.Temperature.Get(device, uuid)
 		if err2 != nil {
-			return fmt.Errorf("failed to get temperature events for device %v: %v", uuid, err2)
+			deviceInfo.PartialErrors = append(deviceInfo.PartialErrors, fmt.Sprintf("failed to get temperature info: %v", err2))
 		}
 	}
 
+	// Get Utilization info
 	err2 = deviceInfo.Utilization.Get(device, uuid)
 	if err2 != nil {
-		return fmt.Errorf("failed to get Utilization for device %v: %v", uuid, err2)
+		deviceInfo.PartialErrors = append(deviceInfo.PartialErrors, fmt.Sprintf("failed to get utilization info: %v", err2))
 	}
 
+	// Get MemoryErrors info
 	err2 = deviceInfo.MemoryErrors.Get(device, uuid)
 	if err2 != nil {
-		return fmt.Errorf("failed to get sensor info for device %v: %v", uuid, err2)
+		deviceInfo.PartialErrors = append(deviceInfo.PartialErrors, fmt.Sprintf("failed to get memory errors info: %v", err2))
 	}
 
+	// Get NVLinkStates info
 	err2 = deviceInfo.NVLinkStates.Get(device, uuid)
 	if err2 != nil {
-		return fmt.Errorf("failed to get nvlink states for device %v: %v", uuid, err2)
+		deviceInfo.PartialErrors = append(deviceInfo.PartialErrors, fmt.Sprintf("failed to get nvlink states: %v", err2))
 	}
+
 	// Get the number of processes using the GPU
 	processInfo, err := device.GetComputeRunningProcesses()
 	if !errors.Is(err, nvml.SUCCESS) {
-		return fmt.Errorf("failed to get processes for GPU %v: %s", uuid, nvml.ErrorString(err))
+		deviceInfo.PartialErrors = append(deviceInfo.PartialErrors, fmt.Sprintf("failed to get processes: %v", nvml.ErrorString(err)))
+		deviceInfo.NProcess = 0
+	} else {
+		deviceInfo.NProcess = len(processInfo)
 	}
-	deviceInfo.NProcess = len(processInfo)
 
+	if len(deviceInfo.PartialErrors) > 0 {
+		return fmt.Errorf("failed to get device info: %d errors occurred", len(deviceInfo.PartialErrors))
+	}
 	return nil
 }
 
